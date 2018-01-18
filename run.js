720091c1ee465382aef0738670593db5314891f4
Jason Howmans <jason@nosaj.io>
2018-01-18 22:25:42 +0000

    Successfully publish test post

--- run.js
+++ run.js
@@ -14,9 +14,19 @@ Blah blah blah
 const debug = require('debug')('mediumbot:init');
 const error = require('debug')('mediumbot:error:init');
 const { MediumClient } = require('medium-sdk');
-const { MEDIUM_ACCESS_TOKEN, MEDIUM_USER_ID } = process.env;
+// Variables from .env file
+const { 
+  MEDIUM_ACCESS_TOKEN, 
+  MEDIUM_USER_ID, 
+  MEDIUM_CLIENT_ID, 
+  MEDIUM_CLIENT_SECRET 
+} = process.env;
 
-const client = new MediumClient();
+
+const client = new MediumClient({clientId: MEDIUM_CLIENT_ID, clientSecret: MEDIUM_CLIENT_SECRET });
+
+client.setAccessToken(MEDIUM_ACCESS_TOKEN);
+
 
 const postData = {
   userId: MEDIUM_USER_ID,
