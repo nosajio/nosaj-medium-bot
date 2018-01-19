3629decbf14ffa0a3ba001da46c78d8e30243030
Jason Howmans <jason@nosaj.io>
2018-01-19 20:58:16 +0000

    In the black

--- blacklist.js
+++ blacklist.js
@@ -12,6 +12,13 @@ const postsBlacklist = [
   'https://nosaj.io/r/replace-patreon',
   'https://nosaj.io/r/how-to-run',
   'https://nosaj.io/r/ripcast-writeup',
+  
+  // The ones below I just don't want to cross-post
+  'https://nosaj.io/r/giving-podcasts-a-makeover',
+  'https://nosaj.io/r/making-a-holiday-website',
+  'https://nosaj.io/r/the-bane-of-notifications',
+  'https://nosaj.io/r/the-fragility-of-education',
+  'https://nosaj.io/r/recreational-outrage-is-toxic',
 ];
  
 module.exports = postsBlacklist;
