6468e61483ea975121ed74b8a069eba59fb0c124
Jason Howmans <jason@nosaj.io>
2018-01-20 11:57:24 +0000

    Clean up footer html

--- post-footer.js
+++ post-footer.js
@@ -2,7 +2,7 @@ const postFooter = `<hr>
 <p>
   <em>This was originally posted on my blog, where I write about the craft of making software and other things that I think people will find valuable. </em>
   <a href="https://nosaj.io" target="_blank"><em>https://nosaj.io.</em></a>
-  <em class="markup--em markup--p-em"> Go and check it out!</em>
+  <em> Go and check it out!</em>
 </p>`;
 
 const appendPostFooter = str => `${str}\n\n${postFooter}`;
